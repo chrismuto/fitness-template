b5c6a7d0d80d4a77b81557143ab7c827b597bc8d
chrismuto <chrismuto@gmail.com>
2024-11-06 19:37:49 -0800

    animated hamburger menu and styled mobile nav menu that appears

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,18 @@
+const menuButton = document.getElementById("menu-button");
+const mobileMenu = document.getElementById("nav-menu");
+
+menuButton.addEventListener("click", () => {
+    if (!mobileMenu.classList.contains("menu-slide-in")) {
+        mobileMenu.classList.remove("menu-slide-out");
+        mobileMenu.classList.add("menu-slide-in");
+        menuButton.src = "./assets/images/close-menu.png";
+    } else {
+        menuButton.src = "./assets/images/hamburger-menu.svg";
+        mobileMenu.classList.remove("menu-slide-in");
+        mobileMenu.classList.add("menu-slide-out");
+    };
+});
+
 //Trigger CSS animations when elements scroll into view
 //uses Intersection Observer API to determine if an object is in the viewport and adds in-view class to elements as they enter, and can remove class when the element leaves
 //use this to add keyframes and transition animations to any elements with the .animate class
